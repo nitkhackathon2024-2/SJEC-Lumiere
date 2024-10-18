acb9a362a58e8b1eff9f17d501d001ff1edc3d42
pranavvraja <pranavvraja@gmail.com>
2024-10-18 16:05:54 +0530

    add: client form page

--- code/client/src/app/form/page.tsx
+++ code/client/src/app/form/page.tsx
@@ -20,14 +20,11 @@ const formData = [
       dob: "1990-05-12",
       gender: "Male",
       aadhar_number: "1234 5678 9101",
-      address: {
-        street: "123, MG Road",
-        city: "Bangalore",
-        state: "Karnataka",
-        pincode: "560001",
-      },
+      address: "123, MG Road Bangalore Karnataka 560001",
+      phone: "23543i03",
     },
   },
+
   {
     type: "PAN Card",
     data: {
@@ -44,12 +41,7 @@ const formData = [
       date: "2024-10-10",
       time: "15:45",
       pump_name: "ABC Fuel Station",
-      address: {
-        street: "456, Ring Road",
-        city: "Bangalore",
-        state: "Karnataka",
-        pincode: "560002",
-      },
+      address: "123, MG Road Bangalore Karnataka 560001",
       fuel_type: "Petrol",
       quantity_liters: 20,
       price_per_liter: 102.5,
@@ -66,12 +58,7 @@ const formData = [
       due_date: "2024-10-25",
       units_consumed: 150,
       amount_due: 1350,
-      address: {
-        street: "123, MG Road",
-        city: "Bangalore",
-        state: "Karnataka",
-        pincode: "560001",
-      },
+      address: "123, MG Road Bangalore Karnataka 560001",
     },
   },
   {
@@ -84,12 +71,7 @@ const formData = [
       due_date: "2024-10-30",
       units_consumed_cubic_meters: 30,
       amount_due: 500,
-      address: {
-        street: "123, MG Road",
-        city: "Bangalore",
-        state: "Karnataka",
-        pincode: "560001",
-      },
+      address: "123, MG Road Bangalore Karnataka 560001",
     },
   },
   {
@@ -100,17 +82,13 @@ const formData = [
       license_number: "DL1234567890",
       issue_date: "2015-05-12",
       expiry_date: "2035-05-12",
-      address: {
-        street: "123, MG Road",
-        city: "Bangalore",
-        state: "Karnataka",
-        pincode: "560001",
-      },
+      address: "123, MG Road Bangalore Karnataka 560001",
     },
   },
 ];
 
 export default function Form() {
+  console.log("data", formData[0].data.name);
   return (
     <main className="flex items-center w-full h-screen justify-center">
       <Tabs defaultValue={formData[0].type} className="w-fit">
@@ -127,22 +105,24 @@ export default function Form() {
               <Card>
                 <CardHeader>
                   <CardTitle>{form.type}</CardTitle>
-                  {/* <CardDescription>
-                    Make changes to your account here. Click save when
-                    you&apos;re done.
-                  </CardDescription> */}
                 </CardHeader>
                 <CardContent className="space-y-2">
-                  {formData.map((fields) => (
-                    <div className="space-y-1" key={fields}>
-                      <Label htmlFor="name">Name</Label>
-                      <Input id="name" defaultValue="Pedro Duarte" />
+                  {/* {formData.map((fields) => ( */}
+                  {Object.entries(form.data).map(([key, value]) => (
+                    <div className="space-y-1" key={key}>
+                      <Label htmlFor="name">{key.toUpperCase()}</Label>
+                      <Input id="name" defaultValue={value} />
                     </div>
                   ))}
-                  <div className="space-y-1">
+
+                  {/* // <div key={key}> */}
+                  {/* <strong>{key}:</strong> {value} */}
+                  {/* </div> */}
+                  {/* // ))} */}
+                  {/* <div className="space-y-1">
                     <Label htmlFor="username">Username</Label>
                     <Input id="username" defaultValue="@peduarte" />
-                  </div>
+                  </div> */}
                 </CardContent>
                 <CardFooter>
                   <Button>Save changes</Button>
