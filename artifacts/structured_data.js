12a14eb294a22f6888ed434ab80fd4f66d1ee001
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-10-19 08:43:54 +0530

    added: workflow diagram and problem statement

--- artifacts/structured_data.js
+++ artifacts/structured_data.js
@@ -1,14 +1,15 @@
 export const data = [
     {
-        "type": "driver's_license",
+        "type": "driver",
         "data": {
-            "name": "PRATHAM V BHAT",
-            "date_of_birth": "05/12/2003",
-            "birth_group": "Bt",
-            "valid_till": "04/12/2043",
-            "dl_number": "KA19 20240004325",
-            "doi": "10/05/2024",
-            "address": "6-144/2-3 BRINDAVANA KAMALAMMA BADAVANE CAPITANIO BEHIND CAPITANIO SCHOOL POST KANKANADY ae a Mangalore,Dakshina Kannad,KA 575002"
+            "DL Number": "KA19 20240004325",
+            "DOI": "10/05/2024",
+            "Name": "PRATHAM V BHAT",
+            "Date of Birth": "05/12/2003",
+            "Blood Group": "Bt",
+            "Valid Till": "04/12/2043(NT)",
+            "Address": "6-144/2-3 BRINDAVANA KAMALAMMA BADAVANE CAPITANIO BEHIND CAPITANIO SCHOOL POST KANKANADY ae a Mangalore,Dakshina Kannad,KA 575002",
+            "Father's Name": "S VENKATESH BHAT"
         }
     }
 ];
